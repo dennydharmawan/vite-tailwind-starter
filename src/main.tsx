2bd16cb5373a150a05a2e5d8efcb7229fab45284
denny dharmawan <denny.dharmawan@finaccel.id>
2023-10-31 16:53:29 +0700

    init project

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
+import { MantineProvider, createTheme } from '@mantine/core';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
+
+import '@mantine/core/styles.css';
 import './index.css';
-import { MantineProvider } from '@mantine/core';
+
+const theme = createTheme({
+  /** Put your mantine theme override here */
+  primaryColor: 'blue',
+});
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <MantineProvider withNormalizeCSS>
+  <MantineProvider theme={theme}>
     <App />
   </MantineProvider>,
 );
