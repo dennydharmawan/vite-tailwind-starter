471795d77e98d06b73cc094cff754784bec21466
denny dharmawan <denny.dharmawan@krom.id>
2023-12-21 13:52:07 +0700

    polished eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,17 +3,26 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
+    'plugin:typescript-paths/recommended',
     'plugin:react/recommended',
-    'plugin:react/jsx-runtime'
+    'plugin:react-hooks/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:tailwindcss/recommended'
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  plugins: ['react-refresh', 'tailwindcss'],
+  plugins: ['@typescript-eslint', 'import', 'react-refresh'],
   rules: {
     'react-refresh/only-export-components': 'warn',
+    '@typescript-eslint/consistent-type-exports': 'error',
+    '@typescript-eslint/consistent-type-imports': 'error',
+    'import/consistent-type-specifier-style': ['error', 'prefer-inline'],
+    'import/no-duplicates': 'error',
+    'import/no-extraneous-dependencies': 'off',
+    'import/no-cycle': 'error',
     'react/jsx-uses-react': 'off',
-    'react/react-in-jsx-scope': 'off'
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'off'
   },
   settings: {
     react: {
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,7 @@ export default function index() {
   return (
     <div className="super-centered min-h-screen bg-red-100">
       Hello world <Button variant="filled">test</Button>
-      <p className="max-w-[65ch]">
+      <p className="max-w-prose">
         Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ex fuga, temporibus ad ipsam consectetur
         ipsa voluptas, quis voluptatum error natus, eaque atque. Optio, libero! Neque excepturi cupiditate
         consequuntur nostrum laboriosam.
