4de202af9af2b6e86d1764cbab625a088b1d88c6
denny dharmawan <denny.dharmawan@krom.id>
2023-12-21 14:01:29 +0700

    removed unused comment

--- src/lib/mantine.ts
+++ src/lib/mantine.ts
@@ -9,21 +9,6 @@ type _TupleOf<T, N extends number, R extends unknown[]> = R['length'] extends N
   ? R
   : _TupleOf<T, N, [T, ...R]>;
 
-// type TailwindColor = {
-//   25?: string;
-//   50: string;
-//   100: string;
-//   200: string;
-//   300: string;
-//   400: string;
-//   500: string;
-//   600: string;
-//   700: string;
-//   800: string;
-//   900: string;
-//   950?: string;
-// };
-
 type CustomColors = keyof typeof colors;
 
 declare module '@mantine/core' {
