5a7d431ec14d25b2d84625c9c0a06e9f715dd57e
denny dharmawan <denny.dharmawan@krom.id>
2024-01-08 16:58:25 +0700

    changed a bunch of files

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,7 +3,6 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
-    'plugin:typescript-paths/recommended',
     'plugin:react/recommended',
     'plugin:react-hooks/recommended',
     'plugin:react/jsx-runtime',
--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,11 @@
-import path from 'path';
 import { defineConfig } from 'vite';
 import svgr from 'vite-plugin-svgr';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 import generouted from '@generouted/react-router/plugin';
 import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src')
-    }
-  },
-  plugins: [svgr(), react(), generouted()]
+  plugins: [react(), tsconfigPaths(), svgr(), generouted()]
 });
